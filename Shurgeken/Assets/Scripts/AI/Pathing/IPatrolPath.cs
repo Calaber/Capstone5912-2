c5576d5a9901db03ef63d2b2abd3bb190d2835f1
Kris Wenger <wenger.107@osu.edu>
2017-02-14 23:02:47 -0500

    Enemy AI Attack State, FOV, and Rand Pathing
    Added Attack State, need player contoller to make it actually hurt
    player, FOV, need to add lighting effecting vision in again, and Rand
    Pathing, think this is complete.
    
    Need To Do:
    Clean Up Code
    Add model and animations for master only not for networking yet.
    Rework Flag Vision

---
+++ Shurgeken/Assets/Scripts/AI/Pathing/IPatrolPath.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPatrolPath {
+
+    Transform[] getPath();
+
+    void setNumberOfWayPoints(int waypoints);
+
+    void randamizePath();
+}
