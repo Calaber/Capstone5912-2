74867144319d324c875dae110ac23cd8247f6fd1
Calaber <maah.1@osu.edu>
2017-02-15 02:23:17 -0500

    audio slider

---
+++ Shurgeken/Assets/Scripts/UI/MasterAudioSlide.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasterAudioSlide : MonoBehaviour {
+
+    public Slider volumeSlider;
+
+	// Use this for initialization
+	void Start () {
+        AudioListener.volume = volumeSlider.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AudioListener.volume = volumeSlider.value;
+    }
+}
