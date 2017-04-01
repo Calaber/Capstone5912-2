712f2e3c01688b6de1b2e937516c9d71478a5a7f
Kris Wenger <wenger.107@osu.edu>
2017-04-01 16:02:11 -0400

    Added Solo Play Loading Screen

---
+++ Shurgeken/Assets/Scripts/UI/LoadScreenScripts/LoadSoloPlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSoloPlay : MonoBehaviour {
+    private bool once = false;
+
+    void Awake()
+    {
+        once = false;
+    }
+	// Use this for initialization
+	void Update ()
+    {
+        LoadLevel();
+    }
+
+    private void LoadLevel()
+    {
+        if (!once)
+        {
+            once = true;
+            NetworkManager.networkManager.createRoom("myRoom", NetworkManager.GameType.SINGLE);
+            NetworkManager.networkManager.loadLevel("Demo 6");
+        }
+    }
+}
