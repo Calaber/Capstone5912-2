d1b1e6055abc54262da8d3226b57a9c7dab0e06c
Albert Ma <albertfma@gmail.com>
2017-04-02 11:33:27 -0400

    Invert Y axis enabled

---
+++ Shurgeken/Assets/Scripts/UI/YInversion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YInversion : MonoBehaviour {
+
+    public Dropdown Inversion;
+
+    // Use this for initialization
+    void Start () {
+        setValue();
+	}
+
+
+	void setValue ()
+    {
+        switch (PlayerSettings.Y_Invert)
+        {
+            case true:
+                Inversion.value = 1;
+                break;
+            case false:
+                Inversion.value = 0;
+                break;
+            default:
+                Inversion.value = 0;
+                break;
+        }
+    }
+
+
+    void OnEnable()
+    {
+        setValue();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
