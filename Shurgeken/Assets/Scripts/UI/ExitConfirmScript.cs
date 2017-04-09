ede39af150d7d7ebd4810bfaf0ebdd20e698d88f
Calaber <maah.1@osu.edu>
2017-04-09 19:40:24 -0400

    Exit Screen

---
+++ Shurgeken/Assets/Scripts/UI/ExitConfirmScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitConfirmScript : MonoBehaviour {
+
+    public GameObject ExitScreen;
+    public Button ExitButton;
+
+	// Use this for initialization
+	void Start () {
+        ExitButton.onClick.AddListener(OpenExitConfirmation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OpenExitConfirmation()
+    {
+        ExitScreen.SetActive(true);
+    }
+}
